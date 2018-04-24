5402b9854cb948dd6bda3c39ba93bdaa728ef692
Santahub <pbaustin96@gmail.com>
2018-04-24 17:13:29 +0530

    Good to clone

--- ML.py
+++ ML.py
@@ -17,7 +17,7 @@ def stratified_cv(X, y, clf_class, clf_name, shuffle=True, n_folds=10, **kwargs)
     from sklearn import cross_validation
     import time
     from sklearn.metrics import accuracy_score
-    stratified_k_fold = cross_validation.StratifiedKFold(y, n_folds=n_folds, shuffle=shuffle) #it will have each folds with index of y. Use this index to get corresponding x values 
+    stratified_k_fold = cross_validation.StratifiedKFold(y, n_folds=n_folds, shuffle=shuffle) #it will have each folds with index of y. Use this index to get corresponding x values
     clf = OneVsRestClassifier(clf_class(**kwargs),n_jobs=-1) #n_job for number of CPU to be used. -1 for all.
     y_pred = y.copy()
     start_time=time.time()
@@ -34,13 +34,13 @@ def stratified_cv(X, y, clf_class, clf_name, shuffle=True, n_folds=10, **kwargs)
 def plot_cm(clf, labels = ['b1', 'b2', 'cycl', 'ly', 'sit', 'stand', 'walk']) :
     from sklearn.metrics import confusion_matrix
     cm  = confusion_matrix(y, clf['y_pred'])
-    percent = (cm*100.0)/np.array(np.matrix(cm.sum(axis=1)).T) 
+    percent = (cm*100.0)/np.array(np.matrix(cm.sum(axis=1)).T)
     print ('\nConfusion Matrix Stats : ',clf['name'],clf['acc'],'%')
 
 #    for i, label_i in enumerate(labels):
 #        for j, label_j in enumerate(labels):
 #            print ("%s/%s: %.2f%% (%d/%d)" % (label_i, label_j, (percent[i][j]), cm[i][j], cm[i].sum()))
-    
+
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.grid(b=False)
@@ -52,10 +52,10 @@ def plot_cm(clf, labels = ['b1', 'b2', 'cycl', 'ly', 'sit', 'stand', 'walk']) :
     for x,Y in enumerate(percent):
         for a,b in enumerate(Y):
             if b < 1:
-                continue 
+                continue
             b = round(b,2)
             ax.text(x,a,b,va='center',ha='center',bbox=dict(fc='w',boxstyle='round,pad=1'))
-            
+
     pylab.xlabel('Predicted')
     pylab.ylabel('Actual')
     pylab.savefig('./Results/'+clf['name']+'.png')
@@ -88,7 +88,7 @@ y = np.array(datay)
 
 from sklearn.preprocessing import MultiLabelBinarizer,LabelEncoder
 enc = MultiLabelBinarizer()
-y_train = enc.fit_transform(y_train)
+y_train = enc.fit_transform(y)
 
 
 
@@ -109,10 +109,7 @@ KNC=stratified_cv(X, y, neighbors.KNeighborsClassifier,"K Neighbors Classifier")
 LSV= stratified_cv(X, y, LinearSVC,"Linear SVC") #accuracy 97.68
 #
 ##Plot Confusion Matrix
-plot_cm(GNB)
-plot_cm(RFC)
-plot_cm(KNC)
-plot_cm(LSV)
++
 #
 #
 ##Plot Classifier compare
